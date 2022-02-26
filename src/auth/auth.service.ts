6493dd6d48121e47b250afbb5e5f2c8d1d4b566e
JHC <gusckd57@gmail.com>
2022-02-27 03:15:03 +0900

    [feature] JWT passport, AuthGuard 생성 #3 (#7)
    * add passport npm modules
    
    * add jwt passport npm modules
    
    * add LocalAuthGuard, JwtAuthGuard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,24 +7,33 @@ import { CustomException } from 'src/exception/customException';
 import { Authentication } from 'src/class/Authentication';
 import { UserInfoDto } from 'src/dto/userInfo.dto';
 import { UserInformation } from 'src/user-info/entity/UserInformation';
-import { LoginDataDto } from 'src/dto/loginData.dto';
+import { JwtService } from '@nestjs/jwt';
+import { TokenDto } from 'src/dto/token.dto';
 const {WRONG_LOGIN_DATA} = errors;
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService, 
-    private userInfoService: UserInfoService
+    private userInfoService: UserInfoService,
+    private jwtService: JwtService
     ){}
   
-  async validateUser(loginData:LoginDataDto): Promise<User>{
-    const {username, password} = loginData;
+  async login(username: string, password: string): Promise<Authentication>{
     const user = await this.userService.getUserByUsername(username);
     if(!this.userService.comparePassword(password, user.password)) throw new CustomException(WRONG_LOGIN_DATA);
-    return user;
+    const {password: userPassword, ...result} = user;
+    return result;
   }
 
-  async changePassword(user: Authentication<User>, currentPassword: string, newPassword: string): Promise<User>{
-    const { sub:id } = user;
+  async signToken(user:Authentication): Promise<TokenDto>{
+    return {
+      accessToken: this.jwtService.sign(user),
+      refreshToken: this.jwtService.sign({}),
+    }
+  }
+
+  async changePassword(user: Authentication, currentPassword: string, newPassword: string): Promise<User>{
+    const { id } = user;
     const currentUser = await this.userService.getUserById(id);
     this.userService.comparePassword(currentPassword, currentUser.password);
     this.userService.updatePassword(currentUser, newPassword);
