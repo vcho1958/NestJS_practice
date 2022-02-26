7a6e3f09c2e43e610bb19e0c29044ae398a5ab58
vcho1958 <gusckd57@gmail.com>
2022-02-26 23:07:27 +0900

    mod UserInfoMemoryRepository to extends MeMoryRepository

--- src/user-info/repository/userInfoMemory.repository.ts
+++ src/user-info/repository/userInfoMemory.repository.ts
@@ -1,52 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { UserInfoDto } from 'src/dto/userInfo.dto';
-import { getRepository } from 'typeorm';
+import { DeepPartial, getRepository } from 'typeorm';
 import { UserInformation } from '../entity/UserInformation';
 import { IUserInfoRepository } from '../../interface/IUserInfoRepository';
+import { MemoryRepository } from 'src/class/MemoryRepository';
 @Injectable()
-export class UserInfoMemoryRepository implements IUserInfoRepository {
-  UserInformation: Array<UserInformation>;
+export class UserInfoMemoryRepository extends MemoryRepository<UserInformation> implements IUserInfoRepository {
   constructor() {
-    this.UserInformation = [];
+    super()
   }
-  async find(condition:Object): Promise<UserInformation[]>{
-    return this.UserInformation.filter(user => {
-      for(let key in condition) {
-        if (user[key] === condition[key])return true;
-      }
-      return false;
-    });
-  };
 
-  async findOne(condition:Object): Promise<UserInformation>{
-    return this.UserInformation.find(user => {
-      for(let key in condition) {
-        if (user[key] === condition[key])return true;
-      }
-      return false;
-    });
-  };
-  
-  async findById(id: number): Promise<UserInformation> {
-    return this.UserInformation[id];
-  }
 
   async findByEmail(email: string): Promise<UserInformation>{
-    return this.UserInformation.find(user => user.email === email)
+    return this.Memory.find(user => user.email === email)
   };
 
   async findByPhone(phone: string): Promise<UserInformation>{
-    return this.UserInformation.find(user => user.phone === phone);
+    return this.Memory.find(user => user.phone === phone);
   }
 
 
-  async save(newUserInformation: UserInfoDto | UserInformation): Promise<UserInformation>{
-    const userInformation = getRepository(UserInformation).create(newUserInformation);
-    this.UserInformation.push(userInformation);
-    return userInformation;
-  }
-  async remove(userInformation: UserInformation): Promise<UserInformation> {
-    delete this.UserInformation[userInformation.id];
-    return userInformation;
+  async save(newEntity: UserInformation | DeepPartial<UserInformation>): Promise<UserInformation>{
+    const entity = new UserInformation();
+    for(const key in newEntity){
+      entity[key] = newEntity[key];
+    }
+    entity.id = this.Memory.length;
+    this.Memory.push(entity);
+    return entity;
   }
 }
